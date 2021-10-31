45ca2cc51b4c0be738251d84f4e84dd9279506a6
hansvandenpol <hans.van.den.pol@validsign.nl>
2021-10-31 14:03:09 +0100

    added new features

--- src/main/java/shadowclicker/Main.java
+++ src/main/java/shadowclicker/Main.java
@@ -24,7 +24,21 @@ public class Main{
             if(Files.exists(DOC_PATH)) {
             try (Stream<String> stream = Files.lines( DOC_PATH, StandardCharsets.UTF_8)) {
                stream.findFirst().ifPresent(record -> {
-                    if (record.contains(":") && record.contains(";")) {
+                   if(record.contains("RIGHT_CLICK")) {
+                       String type = record.split("-")[0];
+                       final String clickLocation = record.split("-")[1];
+                       int x = Integer.parseInt(clickLocation.split(":")[0]);
+                       int y = Integer.parseInt(clickLocation.split(":")[1].split(";")[0]);
+                       int timeout = Integer.parseInt(clickLocation.split(";")[1]);
+
+                       try {
+                           Util u = new Util();
+                           u.mouseRightClick(x, y);
+                           Thread.sleep(timeout);
+                       } catch (InterruptedException e) {
+                           e.printStackTrace();
+                       }
+                   } else if (record.contains(":") && record.contains(";")) {
                         int x = Integer.parseInt(record.split(":")[0]);
                         int y = Integer.parseInt(record.split(":")[1].split(";")[0]);
                         int timeout = Integer.parseInt(record.split(";")[1]);
@@ -32,6 +46,17 @@ public class Main{
                         try {
                             Util u = new Util();
                             u.mouseClick(x, y, false);
+                            Thread.sleep(timeout);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    } else if(record.contains("TYPE")) {
+                        final String text = record.split(":")[1];
+                        final int timeout = Integer.parseInt(record.split(":")[2]);
+
+                        try {
+                            Util u = new Util();
+                            u.type(text);
                             Thread.sleep(timeout);
                         } catch (InterruptedException e) {
                             e.printStackTrace();
@@ -47,6 +72,14 @@ public class Main{
                             }
                         } else if(record.contains("ESCAPE")) {
                             Util.pressButton(KeyEvent.VK_ESCAPE);
+                            final int timout = Integer.parseInt(record.split(":")[1]);
+                            try {
+                                Thread.sleep(timout);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        } else if(record.contains("ENTER")) {
+                            Util.pressButton(KeyEvent.VK_ENTER);
                             final int timout = Integer.parseInt(record.split(":")[1]);
                             try {
                                 Thread.sleep(timout);
--- src/main/java/shadowclicker/Util.java
+++ src/main/java/shadowclicker/Util.java
@@ -6,6 +6,7 @@ import com.github.joonasvali.naturalmouse.util.FactoryTemplates;
 import java.awt.*;
 import java.awt.event.InputEvent;
 import java.awt.event.KeyEvent;
+import java.util.Locale;
 
 public class Util {
     private static Robot robot;
@@ -28,6 +29,14 @@ public class Util {
         robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
     }
 
+    public void rightClick(){
+        robot.mousePress(InputEvent.BUTTON3_DOWN_MASK);
+
+        robot.delay((int)(Math.random() * 50) + 50);
+
+        robot.mouseRelease(InputEvent.BUTTON3_DOWN_MASK);
+    }
+
     public void mouseClick(int xPos, int yPos, boolean isFast) throws InterruptedException {
         int speed = 200;
         int offset = 100;
@@ -41,9 +50,29 @@ public class Util {
         click();
     }
 
+    public void mouseRightClick(int xPos, int yPos) throws InterruptedException {
+        int speed = 200;
+        int offset = 100;
+
+        mouse.move(xPos, yPos);
+        robot.delay((int)(Math.random() * offset) + speed);
+        rightClick();
+    }
+
     public static void pressButton(int key) {
         robot.keyPress(key);
         robot.delay((int)(Math.random() * 50) + 100);
         robot.keyRelease(key);
+    }
+
+    public void type(String text) {
+        text = text.toLowerCase(Locale.ROOT);
+
+        for(char c : text.toCharArray()) {
+            int keyCode = KeyEvent.getExtendedKeyCodeForChar(c);
+            robot.keyPress(keyCode);
+            robot.delay((int)(Math.random() * 50) + 300);
+            robot.keyRelease(keyCode);
+        }
     }
 }
